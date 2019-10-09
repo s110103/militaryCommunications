e9c7eb3e3821b26e4b60be04137fd80cbd720664
lukas <lukasschauer2704@gmail.com>
2019-10-09 13:09:01 +0200

    /editLetters /inventLetter /readLetters /approveUser /editUser /registerUser /index

--- Blub/letters/inventLetter.php
+++ Blub/letters/inventLetter.php
@@ -31,7 +31,7 @@ include "../checkSession.php";
     <!-- ============================================================== -->
     <div class="dashboard-header">
         <nav class="navbar navbar-expand-lg bg-white fixed-top">
-            <a class="navbar-brand" href="../index.php">military communications</a>
+            <a class="navbar-brand" href="../index.php">MCS</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
